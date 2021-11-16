522e1608764c845416d395573143c6a6d94beacb
Neil Dela Cruz <genoznightmare@gmail.com>
2021-11-16 23:44:44 +0800

    mobile view first main__hero

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$( '.dropdownMenu a' ).on("click", function(){
+    $('.dropdownMenu').hide();
+    });
+
+
+   const menu = document.getElementById('dropdownMenu');
+
+   menu.addEventListener(clicked, function(){
+       menu.hide()
+   })
